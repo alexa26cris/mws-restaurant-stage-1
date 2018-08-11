bfde6917393c43c679ef2875590e3d7037b3309c
Alexandra Crisan <36537392+alexa26cris@users.noreply.github.com>
2018-08-11 13:54:13 +0200

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiYWxleGFuZHJhY3Jpc2FuMDYiLCJhIjoiY2praTR4ajNmMGM0NDNrcGhpN3F2emJ1byJ9.Sy0kLMZ0oZwJO0-HTHYcNg',
+        mapboxToken: 'pk.eyJ1IjoiY2F0aGVyaW5lLWhhZGppaXNrYSIsImEiOiJjamthcTZqdWowcjN1M3JwbnA1Ym95NTV5In0.F-UJMktp9cuvxTHfIrmiPw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -89,7 +89,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  image.alt = restaurant.name;
+ 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
