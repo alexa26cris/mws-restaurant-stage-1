7395b02c9b773cf8d9001fb28c589ebcc2d1610d
Alexandra Crisan <36537392+alexa26cris@users.noreply.github.com>
2018-08-14 12:01:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,57 @@
-var filesToCache = [
-'./', 
-'./css/styles.css',
-'./data/restaurants.json',
-'./img/1.jpg',
-'./img/2.jpg',
-'./img/3.jpg', 
-'./img/4.jpg',
-'./img/5.jpg',
-'./img/6.jpg',
-'./img/7.jpg',
-'./img/8.jpg', 
-'./img/9.jpg',
-'./img/10.jpg',
-'./js/dbhelper.js',
-'./js/restaurant_info.js',
-'./js/main.js',
-'./index.html',
-'./restaurant.html',
+var staticCasheName = 'restaurant-cashe-1';
+let urlToCashe = [
+  './',
+  './restaurant.html',
+  './index.html',
+  './js/restaurant_info.js',
+  './js/main.js',
+  './js/dbhelper',
+  './js/sw/sw.js',
+  './data/restaurants.json',
+  './css/styles.css',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg'
 ];
 
-var staticCacheName = 'pages-cache-v1';
-
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(staticCacheName)
-		.then(function(cache) {
-			return cache.addAll(filesToCache);
-		})
-		);
+self.addEventListener('install',function(event){
+event.waitUnttill(
+  cashes.open(staticCasheName).then(function(cashe){
+  console.log('cashe');
+  return (error)=>{
+    console.log(error);
+  };
+  });
+);
 });
 
-self.addEventListener('fetch', event => {
-	event.respondWith(
-		filesToCache.match(event.request, { ignoreSearch: true }).then(response => {return response || fetch(event.request);
-		}).catch(err => console.log(err))
-		);
+self.addEventListener('activate',function(event){
+  event.waitUnttill(
+    cashes.keys().then(function(casheNames){
+      return promise.all(
+        cashe.filter(function(casheName){
+          return casheName.startWith('restaurant-')&&
+          casheName != staticCasheName;
+        }).map(function(casheName){
+          return cashes.delete(casheName);
+        })
+      );
+    })
+  );
+});
+
+// fetch Data
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      chases.match(event.request).then(function(response){
+        return response || fetch (event.request);
+      });
+    );
 });
